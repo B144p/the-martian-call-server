f6108553d3d1f448fbef0b87ea0d9d785f152ba5
bt-144p <gug321gug@gmail.com>
2026-04-11 18:07:49 +0700

    feat(bootstrap): wire up app module and configure NestJS pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { validateEnv } from './config/env.validation';
+import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
+import { PrismaModule } from './prisma/prisma.module';
+import { PusherModule } from './pusher/pusher.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { MessagesModule } from './modules/messages/messages.module';
+import { SignalLogModule } from './modules/signal-log/signal-log.module';
+import { StatsModule } from './modules/stats/stats.module';
+import { CronModule } from './modules/cron/cron.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, validate: validateEnv }),
+    PrismaModule,
+    PusherModule,
+    AuthModule,
+    UsersModule,
+    MessagesModule,
+    SignalLogModule,
+    StatsModule,
+    CronModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,51 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
 import { AppModule } from './app.module';
+import { Env } from './config/env.validation';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = app.get(ConfigService<Env, true>);
+
+  app.setGlobalPrefix('api/v1');
+
+  // Needed for Pusher auth (sends application/x-www-form-urlencoded)
+  app.use(express.urlencoded({ extended: false }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.enableCors({
+    origin: config.get('FRONTEND_URL'),
+    credentials: true,
+  });
+
+  if (config.get('NODE_ENV') !== 'production') {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('The Martian Call')
+      .setDescription('Real-time signal transmission API')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api/docs', app, document);
+  }
+
+  await app.listen(config.get('PORT'));
 }
+
 bootstrap();
